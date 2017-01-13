9aab80072880a01fb7a7010449b1eaa979ced877
samblackmore <samblackmore07@gmail.com>
2017-01-12 20:30:21 -0500

    movement and color :dizzy: :rainbow:

--- sketch.js
+++ sketch.js
@@ -2,8 +2,8 @@ var player, theGround;
 
 function setup() {
   createCanvas(720, 400);
-  player = new Ball(300, 100, 32)
-  theGround = new Ground(100, 300, 500, 32)
+  player = new Ball(300, 100, 32);
+  theGround = new Ground(0, 360, width, 100);
 }
 
 function Ball(x, y, r) {
@@ -11,38 +11,59 @@ function Ball(x, y, r) {
   this.y = y;
   this.r = r;
   this.g = 0.8;
+  this.a = 0.8;
   this.vv = 0;
+  this.vh = 0;
   this.vvMax = 10;
+  this.vhMax = 7;
   this.yStart = y;
+  this.hue = 0;
+  this.colMax = 100;
 
   this.draw = function() {
-    fill(255, 204, 0);
+    if (this.hue > this.colMax) this.hue = 0;
+    colorMode(HSB, this.colMax);
+    fill(this.hue, 90, 70);
     ellipse(this.x, this.y, this.r, this.r);
-  }
+  };
   this.onGround = function() {
     return Math.round(this.y + this.r / 2) === theGround.y;
-  }
+  };
   this.inGround = function() {
     return Math.round(this.y + this.r / 2) > theGround.y;
-  }
+  };
   this.inAir = function() {
     return !(this.onGround() || this.inGround());
-  }
-  this.fall = function() {
+  };
+
+  this.move = function() {
+    this.handleKeys();
+    this.x += this.vh;
+    this.vh /= 1.2;
     if (this.inAir()) this.y += this.vv;
+    if (this.onGround()) this.vv = 0;
     if (this.vv < this.vvMax) this.vv += this.g;
     while (this.inGround()) this.y--;
     if (this.y > height) {
       this.vv = 0;
       this.y = this.yStart;
     }
-  }
-}
-
-function keyPressed() {
-  if (keyCode === UP_ARROW) {
-    player.y -= 1;
-    player.vv = -10;
+  };
+  this.handleKeys = function() {
+    if (keyIsDown(UP_ARROW)) {
+      this.y -= 1;
+      this.vv = -10;
+    }
+    if (keyIsDown(LEFT_ARROW)) {
+      if (Math.abs(this.vh) < this.vhMax)
+        this.vh -= this.a;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+      if (Math.abs(this.vh) < this.vhMax)
+        this.vh += this.a;
+    }
+    if (keyIsDown(UP_ARROW) || keyIsDown(LEFT_ARROW) || keyIsDown(RIGHT_ARROW))
+      this.hue++;
   }
 }
 
@@ -53,14 +74,14 @@ function Ground(x, y, w, h) {
   this.h = h;
   this.draw = function() {
     fill(100);
-    rect(this.x, this.y, this.w, this.h)
-  }
+    rect(this.x, this.y, this.w, this.h);
+  };
 }
 
 function draw() {
-  background(200);
-  stroke(50);
-  player.fall();
+  background('rgba(0,0,0,0.01)');
+  noStroke();
+  player.move();
   player.draw();
   theGround.draw();
 }
